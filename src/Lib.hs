527fa5adeb14090908c8588a4f01519d5576155e
marciondg <mgarozzo@est.frba.utn.edu.ar>
2020-06-10 03:04:40 -0300

    Punto 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -34,13 +34,69 @@ Prestarle n millones de dólares al país, esto provoca que el país se endeude 
 -}
 type EstrategiaFMI = Pais -> Pais
 prestarMillonesDolares :: Dinero->EstrategiaFMI
-prestarMillonesDolares n = endeudar (n*1000000*1.5) 
+prestarMillonesDolares n = endeudar $ ((*1.5).millones) n
+
+cambiarDeuda :: (Dinero->Dinero->Dinero)->Dinero->Pais->Pais
+cambiarDeuda f dinero pais = pais {deudaConFMI = (deudaConFMI pais) `f` dinero}
 
 endeudar :: Dinero->Pais->Pais
-endeudar deuda pais = pais {deudaConFMI = deudaConFMI pais + deuda}
+endeudar dinero = cambiarDeuda (+) dinero
 
+millones :: Dinero -> Dinero
+millones = (*1000000)
 {-+-+-+-+-+-+-+-+-+-+-+-+
-reducir x cantidad de puestos de trabajo del sector público, lo que provoca que se reduzca la cantidad de activos en el sector público y 
+Reducir x cantidad de puestos de trabajo del sector público, lo que provoca que se reduzca la cantidad de activos en el sector público y 
 además que el ingreso per cápita disminuya en 20% si los puestos de trabajo son más de 100 ó 15% en caso contrario
 -}
 
+reducirPuestosTrabajoPublico :: Int->EstrategiaFMI
+reducirPuestosTrabajoPublico puestosAQuitar = (reducirIngresoPerCapita puestosAQuitar.reducirActivosPublico puestosAQuitar) 
+
+reducirActivosPublico :: Int->Pais->Pais
+reducirActivosPublico puestos pais = pais {poblacionActivaPublico = poblacionActivaPublico pais - puestos}
+
+reducirIngresoPerCapita :: Int->Pais->Pais
+reducirIngresoPerCapita puestos pais | puestos > 100 = pais {ingresoPerCapita = ((*0.8).ingresoPerCapita) pais}
+                                     | otherwise = pais {ingresoPerCapita = ((*0.75).ingresoPerCapita) pais}
+
+{-+-+-+-+-+-+-+-+-+-+-+-+
+Darle a una empresa afín al FMI la explotación de alguno de los recursos naturales, 
+esto disminuye 2 millones de dólares la deuda que el país mantiene con el FMI pero también deja momentáneamente 
+sin recurso natural a dicho país. No considerar qué pasa si el país no tiene dicho recurso. 
+-}
+
+permitirExplotacionDeRecursoNatural :: Recurso->EstrategiaFMI
+permitirExplotacionDeRecursoNatural recurso = (regularizarDeuda $ millones 2).perderRecurso recurso
+
+regularizarDeuda :: Dinero->Pais->Pais
+regularizarDeuda dinero = cambiarDeuda (-) dinero
+
+perderRecurso :: Recurso->Pais->Pais
+perderRecurso recurso pais = pais {recursosNaturales= restoRecursos recurso (recursosNaturales pais)}
+
+restoRecursos :: Recurso->[Recurso]->[Recurso]
+restoRecursos recursoASacar listaRecursos = filter (recursoDiferente recursoASacar) listaRecursos
+
+recursoDiferente :: Recurso->Recurso->Bool
+recursoDiferente recurso1 recurso2 = recurso1 /= recurso2
+
+{-+-+-+-+-+-+-+-+-+-+-+-+
+Establecer un “blindaje”, lo que provoca prestarle a dicho país la mitad de su Producto Bruto Interno 
+(que se calcula como el ingreso per cápita multiplicado por su población activa, sumando puestos públicos y privados de trabajo) 
+y reducir 500 puestos de trabajo del sector público. Evitar la repetición de código. -}
+
+darBlindaje :: EstrategiaFMI
+darBlindaje pais = (reducirPuestosTrabajoPublico 500.prestarMillonesDolares(pbi pais / millones 2)) pais
+
+
+pbi :: Pais->Dinero
+pbi pais = ingresoPerCapita pais * fromIntegral(poblacionActiva pais)
+
+poblacionActiva :: Pais->Int
+poblacionActiva pais = poblacionActivaPrivado pais + poblacionActivaPublico pais
+
+{- 
+========Punto 3========
+Modelar una receta que consista en prestar 200 millones, y darle a una empresa X la explotación de la “Minería” de un país.
+Ahora queremos aplicar la receta del punto 3.a al país Namibia (creado en el punto 1.b). Justificar cómo se logra el efecto colateral.
+ -}
